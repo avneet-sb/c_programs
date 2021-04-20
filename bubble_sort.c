1b7aaf164619b33f1f6984afcbb8ab2768d51aa3
sahil <souravjangar91@gmail.com>
2021-04-20 14:59:39 +0530

    binary search added

---
+++ bubble_sort.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#define size 100
+void bubble_sort(int arr[], int num);
+void main(void)
+{
+    int arr[size], num;
+    printf("enter  the number of elements you want to insert :\n");
+    scanf("%d", &num);
+    for (int i = 0; i < num; i++)
+    {
+        printf("enter the element %d\t", i + 1);
+        scanf("%d", &arr[i]);
+    }
+    bubble_sort(arr, num);
+    printf("your sorted array or list:\n");
+    for (int i = 0; i < num; i++)
+        printf("%d\t", arr[i]);
+}
+void bubble_sort(int arr[], int num)
+{
+    int find, temp;
+    for (int i = 0; i < num - 1; i++)
+    {
+        int xchanges = 0;
+        for (int j = 0; j < num - 1 - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                xchanges++;
+            }
+        }
+        if (xchanges == 0)
+            break;
+    }
+}
